fa576388c8372086afe374bfc47f01dd1cfd1249
Rahma <rahmakhattab07@gmail.com>
2024-05-15 19:55:36 +0300

    changed files locations.

--- water_quality.py
+++ water_quality.py
@@ -102,7 +102,7 @@ with st.sidebar:
 if choose == 'About':
     st.write('### Water Potability System About:')
     st.write('---')
-    st.write("##### Access to safe drinking-water is essential to health, a basic human right and a component of effective policy for health protection.\n ##### This is important as a health and development issue at a national, regional, and local level.\n ##### In some regions, it has been shown that investments in water supply and sanitation can yield a net economic benefit, since the reductions in adverse health effects and health care costs outweigh the costs of undertaking the interventions.\n ##### This classification project involves building a model to evaluate water potability to help you access safe drinking water. 🚰💙")
+    st.write("##### Access to safe drinking water is essential to health, a basic human right and a component of effective policy for health protection.\n ##### This is important as a health and development issue at a national, regional, and local level.\n ##### In some regions, it has been shown that investments in water supply and sanitation can yield a net economic benefit, since the reductions in adverse health effects and health care costs outweigh the costs of undertaking the interventions.\n ##### This classification project involves building a model to evaluate water potability to help you access safe drinking water. 🚰💙")
 
 
 # Predictions Page
